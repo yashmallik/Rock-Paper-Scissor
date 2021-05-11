2870e5789bf6b4bf2f2caf9b473f6f9262ea333f
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-05-11 21:36:17 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="$_POST">
+<label for="username">USERNAME</label>
+<input type="text" name="who" id="username" ><br>
+<label for="key">PASSWORD</label>
+<input type="password" name="pass" id="key"><br>
+<input type="submit" value="Log In">
+<input type="submit" value="Cancel"
+</form>
+    
+</body>
+</html>
