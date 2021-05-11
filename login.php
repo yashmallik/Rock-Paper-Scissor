77e672d315319887d8e8d8c4b79b03c42db2158f
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-05-11 22:46:11 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,11 @@
 <?php
+$traceback='';
+if(isset($_POST["who"]) && isset($_POST["pass"])){
+if(strlen($_POST["who"])< 1 || strlen($_POST["pass"]) < 1) {
+    $traceback="User name and password are required";
+    
+}
+}
 
 ?>
 <!DOCTYPE html>
@@ -10,13 +17,19 @@
     <title>Document</title>
 </head>
 <body>
-<form method="$_POST">
+<form method="POST">
+<h2>Please log in</h2>
+<?php
+ if (strlen($traceback) > 0) {
+     echo("<p style='color: red'>".htmlentities($traceback)."</p><br>");
+ }
+?>
 <label for="username">USERNAME</label>
 <input type="text" name="who" id="username" ><br>
 <label for="key">PASSWORD</label>
 <input type="password" name="pass" id="key"><br>
 <input type="submit" value="Log In">
-<input type="submit" value="Cancel"
+<input type="submit" value="Cancel">
 </form>
     
 </body>
