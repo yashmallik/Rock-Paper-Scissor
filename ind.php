3063afac1e5b3ed1b98bca048aa4e1f0ef4100ea
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-05-15 21:10:18 +0530

    Add files via upload

---
+++ ind.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My game</title>
+    <link rel="stylesheet" href="style_index.css">
+</head>
+<body>
+<h1>Stone Paper Scissor</h1>
+    <p><b>Note:</b>If you want to play game please login first</p>
+    <a href="login.php">Log In Please</a>
+    <p>Don't attempt to play the game  <a href="game.php">stone paper scissor</a> without loging it, It will fail.</p>
+
+</body>
+</html>
