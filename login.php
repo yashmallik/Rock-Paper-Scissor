ae8f8ee39d4847e0d1eaac7f2b3a592b652e33d4
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-05-20 18:30:10 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['cancel'])) {
-    header('location:http://localhost/index.php');
+    header('location:ind.php');
     return;
 }
 $traceback='';
@@ -25,23 +25,26 @@ if(isset($_POST["who"]) && isset($_POST["pass"])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>    <title>Log In</title>
+<head>
+    <title>Log In</title>
 </head>
 <body>
+<link rel="stylesheet" href="style/style_login.css">
 <h2>Please log in</h2>
 <?php
  if (strlen($traceback) > 0) {
      echo("<p style='color: red'>".htmlentities($traceback)."</p><br>");
  }
-?>
+?> 
+
 <form method="post">
 <label for="username">USERNAME</label>
-<input type="text" name="who" id="username" ><br>
+<input type="text" name="who" id="username" class="box" ><br>
 <label for="key">PASSWORD</label>
-<input type="password" name="pass" id="key"><br>
-<input type="submit" value="Log In">
-<input type="submit" value="Cancel">
+<input type="password" name="pass" id="key" class="box" >
+<input type="submit" value="Log In" class="button">
+<input type="submit" value="Cancel" name="cancel" class="button" >
 </form>
-    
+
 </body>
 </html>
